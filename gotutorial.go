e408f4fd51f888ff5aa59fa16b8ba7c3229d16d7
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-01-22 22:43:24 +0100

    learnt loops

--- gotutorial.go
+++ gotutorial.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	f "fmt"
 	"log"
+	"math/rand"
 	"os"
-	"strconv"
 	"strings"
+	"time"
 )
 
 var pl = f.Println
@@ -40,33 +41,111 @@ func main() {
 
 	// fmt.Printf("%s %d %c %f %t %o %x\n", "Stuff", 1, 'A', 3.14, true, 1, 1)
 
-	pl("What is your age?")
+	// pl("What is your age?")
+	// reader := bufio.NewReader(os.Stdin)
+	// age, err := reader.ReadString('\n')
+	// if err == nil {
+	// 	age = strings.TrimSpace(age)
+	// 	iAge, err := strconv.Atoi(age)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	if iAge < 5 {
+	// 		fmt.Println("Too young for school")
+	// 	}
+	// 	if iAge == 5 {
+	// 		fmt.Println("Go to Kindergarten")
+	// 	}
+
+	// 	if (iAge > 5) && (iAge <= 17) {
+	// 		grade := iAge - 5
+	// 		fmt.Printf("Go to grade  %d \n", grade)
+	// 	}
+
+	// 	if iAge > 17 {
+	// 		fmt.Println("Go to college")
+	// 	}
+
+	// } else {
+	// 	log.Fatal(err)
+	// }
+
+	// pl("Enter number 1")
+	// reader := bufio.NewReader(os.Stdin)
+	// num1, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// numString := strings.TrimSpace(num1)
+	// number1, err := strconv.Atoi(numString)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// pl("Enter number 2")
+	// num2, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// num2String := strings.TrimSpace(num2)
+	// number2, err := strconv.Atoi(num2String)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("The sum of %d and %d is %d", number1, number2, number1+number2)
+
+	// for x := 1; x < 5; x++ {
+	// 	pl(x)
+	// }
+
+	// fx := 0
+	// for fx <= 5 {
+	// 	pl(fx)
+	// 	fx++
+	// }
+
+	// aNums := []int{1, 2, 3}
+	// for _, num := range aNums {
+	// 	pl(num)
+	// }
+
+	// xVal := 1
+	// for true {
+	// 	if xVal == 5 {
+	// 		break
+	// 	}
+	// 	pl(xVal)
+	// 	xVal++
+	// }
+
 	reader := bufio.NewReader(os.Stdin)
-	age, err := reader.ReadString('\n')
-	if err == nil {
-		age = strings.TrimSpace(age)
-		iAge, err := strconv.Atoi(age)
+	seedSecs := time.Now().Unix()
+	rand.Seed(seedSecs)
+	randNum := rand.Intn(50) + 1
+
+	for true {
+		fmt.Println("Guess a number between 1 and 50")
+		pl("Random number is : ", randNum)
+		guess, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		if iAge < 5 {
-			fmt.Println("Too young for school")
+		guess = strings.TrimSpace(guess)
+		iGuess, err := strConv.Atoi(guess)
+		if err != nil {
+			log.Fatal(err)
 		}
-		if iAge == 5 {
-			fmt.Println("Go to Kindergarten")
-		}
-
-		if (iAge > 5) && (iAge <= 17) {
-			grade := iAge - 5
-			fmt.Printf("Go to grade  %d \n", grade)
+		if iGuess > randNum {
+			fmt.Println("Guess something lower")
 		}
 
-		if iAge > 17 {
-			fmt.Println("Go to college")
+		if iGuess < randNum {
+			fmt.Println("Guess something higher")
 		}
-
-	} else {
-		log.Fatal(err)
+		if iGuess == randNum {
+			fmt.Println("YOU guessed it!")
+			break
+		}
 	}
 
 }
